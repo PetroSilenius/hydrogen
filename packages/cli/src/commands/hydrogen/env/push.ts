91bf259986e2de9a327dba1a3034d9bf1e67a439
Ben Wolfram <ben.wolfram@shopify.com>
2024-01-24 15:25:47 -0500

    remove CI logic

--- packages/cli/src/commands/hydrogen/env/push.ts
+++ packages/cli/src/commands/hydrogen/env/push.ts
@@ -65,6 +65,8 @@ export async function runEnvPush({
   environment,
   path = process.cwd(),
 }: Flags) {
+  let validated: Partial<Environment> = {};
+
   // Ensure .env file exists before anything
   const dotEnvPath = resolvePath(path, '.env');
   if (!fileExists(dotEnvPath)) {
@@ -75,18 +77,6 @@ export async function runEnvPush({
     process.exit(1);
   }
   const existingEnv = await readFile(dotEnvPath);
-
-  // Read git branch if we're in CI
-  const isCI = ciPlatform().isCI;
-  let validated: Partial<Environment> = {};
-  let gitCommit: GitCommit;
-
-  try {
-    gitCommit = await getLatestGitCommit(path);
-    validated.branch = isCI ? (/HEAD -> ([^,]*)/.exec(gitCommit.refs) || [])[1] : undefined;
-  } catch (error) {
-    outputWarn('Could not retrieve Git history.');
-  }
 
   // Authenticate
   const [{session, config}, cliCommand] = await Promise.all([
@@ -143,85 +133,83 @@ export async function runEnvPush({
   }
 
   // Select and validate an environment, if not passed via the flag
-  if (!isCI) {
-    if (environment) {
-      // If an environment was passed in, ensure the parameter is a valid environment, and unique
-      const matchedEnvironments = environments.filter(({name}) => name === environment);
+  if (environment) {
+    // If an environment was passed in, ensure the parameter is a valid environment, and unique
+    const matchedEnvironments = environments.filter(({name}) => name === environment);
 
-      if (matchedEnvironments.length === 0) {
-        renderWarning({
-          headline: 'Environment not found',
-          body: `We could not find an environment matching the name '${environment}'.`
-        });
-        process.exit(1);
-      } else if (matchedEnvironments.length === 1) {
-        const {id, name, branch, type} = matchedEnvironments[0] ?? {};
-        validated = {id, name, branch, type};
-      } else {
-        // Prompt the user for a selection if there are multiple matches
-        const selection = await renderSelectPrompt({
-          message: `There were multiple environments found with the name ${environment}:`,
-          choices:
-          [
-            ...matchedEnvironments.map(({id, name, branch, type, url}) => ({
-              label: `${name} (${branch}) ${type} ${url}`,
-              value: id,
-            })),
-          ]
-        });
-        const {id, name, branch, type} = matchedEnvironments.find(({id}) => id === selection) ?? {};
-        validated = {id, name, branch, type};
-      }
-    } else {
-      // Environment flag not passed
-      const choices = [
-        ...environments.map(({id, name, branch}) => ({
-          label: branch ? `${name} (${branch})` : name,
-          value: id,
-        })),
-      ];
-
-      const pushToBranchSelection = await renderSelectPrompt({
-        message: 'Select a set of environment variables to overwrite:',
-        choices,
+    if (matchedEnvironments.length === 0) {
+      renderWarning({
+        headline: 'Environment not found',
+        body: `We could not find an environment matching the name '${environment}'.`
       });
-
-      const {id, name, branch, type} = environments.find(({id}) => id === pushToBranchSelection) ?? {};
+      process.exit(1);
+    } else if (matchedEnvironments.length === 1) {
+      const {id, name, branch, type} = matchedEnvironments[0] ?? {};
+      validated = {id, name, branch, type};
+    } else {
+      // Prompt the user for a selection if there are multiple matches
+      const selection = await renderSelectPrompt({
+        message: `There were multiple environments found with the name ${environment}:`,
+        choices:
+        [
+          ...matchedEnvironments.map(({id, name, branch, type, url}) => ({
+            label: `${name} (${branch}) ${type} ${url}`,
+            value: id,
+          })),
+        ]
+      });
+      const {id, name, branch, type} = matchedEnvironments.find(({id}) => id === selection) ?? {};
       validated = {id, name, branch, type};
     }
+  } else {
+    // Environment flag not passed
+    const choices = [
+      ...environments.map(({id, name, branch}) => ({
+        label: branch ? `${name} (${branch})` : name,
+        value: id,
+      })),
+    ];
+
+    const pushToBranchSelection = await renderSelectPrompt({
+      message: 'Select a set of environment variables to overwrite:',
+      choices,
+    });
+
+    const {id, name, branch, type} = environments.find(({id}) => id === pushToBranchSelection) ?? {};
+    validated = {id, name, branch, type};
   }
 
-  // Generate a diff of the changes, and confirm changes
-  if (!isCI && validated.type === 'PRODUCTION' && validated.name) {
-    const {environmentVariables = []} = await getStorefrontEnvVariables(
-      session,
-      config.storefront.id,
-      validated.branch ?? undefined,
-    ) ?? {};
+  // Confirm changes and show a generate diff of changes
+  if (!validated.name) process.exit(1);
 
-    const fetchedEnv = environmentVariables.reduce((acc, {isSecret, key, value}) => {
-      const entry = `${key}=${isSecret ? `EXISTING_SECRET_VALUE` : value}`;
-      return `${acc}${entry}\n`;
-    }, '');
+  const {environmentVariables = []} = await getStorefrontEnvVariables(
+    session,
+    config.storefront.id,
+    validated.branch ?? undefined,
+  ) ?? {};
 
-    if (existingEnv === fetchedEnv) {
-      renderInfo({
-        body: `No changes to your environment variables`,
-      });
-    } else {
-      const diff = diffLines(fetchedEnv, existingEnv);
-      const overwrite = await renderConfirmationPrompt({
-        confirmationMessage: `Yes, confirm changes`,
-        cancellationMessage: `No, make changes later`,
-        message: outputContent`We'll make the following changes to your environment variables for ${validated.name}:
+  const fetchedEnv = environmentVariables.reduce((acc, {isSecret, key, value}) => {
+    const entry = `${key}=${isSecret ? `EXISTING_SECRET_VALUE` : value}`;
+    return `${acc}${entry}\n`;
+  }, '');
 
-  ${outputToken.linesDiff(diff)}
-  Continue?`.value,
-      });
+  if (existingEnv === fetchedEnv) {
+    renderInfo({
+      body: `No changes to your environment variables`,
+    });
+  } else {
+    const diff = diffLines(fetchedEnv, existingEnv);
+    const confirmPush = await renderConfirmationPrompt({
+      confirmationMessage: `Yes, confirm changes`,
+      cancellationMessage: `No, make changes later`,
+      message: outputContent`We'll make the following changes to your environment variables for ${validated.name}:
 
-      // Cancelled making changes
-      if (!overwrite) process.exit(0);
-    }
+${outputToken.linesDiff(diff)}
+Continue?`.value,
+    });
+
+    // Cancelled making changes
+    if (!confirmPush) process.exit(0);
   }
 
   const parsedVars = parseEnvFile(existingEnv)
